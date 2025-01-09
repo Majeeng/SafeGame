5ab2b38881c6bfc49145ec21a9bf05e3adccc116
Alexandr <eph.sasha@gmail.com>
2025-01-09 18:20:04 +0300

    Добавьте файлы проекта.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace SafeGame
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.PanelTopic = new System.Windows.Forms.Panel();
+            this.tbN = new System.Windows.Forms.NumericUpDown();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.labelWins = new System.Windows.Forms.Label();
+            this.labelRotates = new System.Windows.Forms.Label();
+            this.labelN = new System.Windows.Forms.Label();
+            this.MainPanel.SuspendLayout();
+            this.PanelTopic.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tbN)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.AutoScroll = true;
+            this.MainPanel.Controls.Add(this.PanelTopic);
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPanel.Location = new System.Drawing.Point(0, 0);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(700, 450);
+            this.MainPanel.TabIndex = 0;
+            // 
+            // PanelTopic
+            // 
+            this.PanelTopic.Controls.Add(this.tbN);
+            this.PanelTopic.Controls.Add(this.btnRestart);
+            this.PanelTopic.Controls.Add(this.labelWins);
+            this.PanelTopic.Controls.Add(this.labelRotates);
+            this.PanelTopic.Controls.Add(this.labelN);
+            this.PanelTopic.Location = new System.Drawing.Point(74, 74);
+            this.PanelTopic.Name = "PanelTopic";
+            this.PanelTopic.Size = new System.Drawing.Size(577, 60);
+            this.PanelTopic.TabIndex = 3;
+            // 
+            // tbN
+            // 
+            this.tbN.Font = new System.Drawing.Font("Comic Sans MS", 10F);
+            this.tbN.Location = new System.Drawing.Point(437, 4);
+            this.tbN.Name = "tbN";
+            this.tbN.Size = new System.Drawing.Size(71, 31);
+            this.tbN.TabIndex = 4;
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Font = new System.Drawing.Font("Comic Sans MS", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnRestart.Location = new System.Drawing.Point(194, 7);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(150, 50);
+            this.btnRestart.TabIndex = 0;
+            this.btnRestart.Text = "Start";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // labelWins
+            // 
+            this.labelWins.AutoSize = true;
+            this.labelWins.Font = new System.Drawing.Font("Comic Sans MS", 10F);
+            this.labelWins.Location = new System.Drawing.Point(46, 20);
+            this.labelWins.Name = "labelWins";
+            this.labelWins.Size = new System.Drawing.Size(115, 24);
+            this.labelWins.TabIndex = 2;
+            this.labelWins.Text = "Пройдено: 0";
+            // 
+            // labelRotates
+            // 
+            this.labelRotates.AutoSize = true;
+            this.labelRotates.Font = new System.Drawing.Font("Comic Sans MS", 10F);
+            this.labelRotates.Location = new System.Drawing.Point(350, 33);
+            this.labelRotates.Name = "labelRotates";
+            this.labelRotates.Size = new System.Drawing.Size(129, 24);
+            this.labelRotates.TabIndex = 2;
+            this.labelRotates.Text = "Проворотов: 0";
+            // 
+            // labelN
+            // 
+            this.labelN.AutoSize = true;
+            this.labelN.Font = new System.Drawing.Font("Comic Sans MS", 10F);
+            this.labelN.Location = new System.Drawing.Point(350, 7);
+            this.labelN.Name = "labelN";
+            this.labelN.Size = new System.Drawing.Size(87, 24);
+            this.labelN.TabIndex = 2;
+            this.labelN.Text = "Матрица:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(700, 450);
+            this.Controls.Add(this.MainPanel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.MainPanel.ResumeLayout(false);
+            this.PanelTopic.ResumeLayout(false);
+            this.PanelTopic.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tbN)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.Label labelN;
+        private System.Windows.Forms.Label labelRotates;
+        private System.Windows.Forms.Panel PanelTopic;
+        private System.Windows.Forms.Label labelWins;
+        private System.Windows.Forms.NumericUpDown tbN;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SafeGame
+{
+    public partial class MainForm : Form
+    {
+        // Elements
+        int N;
+        int[,] HandleMatrix;
+        int rotateCounter;
+        int counterOfWinsMatrix = 0, restartMatrixCounter = 0;
+
+        // sound
+        SoundPlayer PlaySound = new SoundPlayer(Properties.Resources.skrip);
+
+        // To create picture boxes
+        int x, y;
+        PictureBox pBox;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            CenterControlInParent(PanelTopic);
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Int32.Parse(tbN.Value.ToString()) <= 0)
+                    MessageBox.Show("Размер матрицы не может быть 0 или меньше.");
+                else
+                {
+                    btnRestart.Text = "Restart";
+
+                    // Clear picture boxes
+                    MainPanel.Controls.OfType<PictureBox>().ToList().ForEach(pb => pb.Dispose());
+
+                    // Get values
+                    N = Int32.Parse(tbN.Value.ToString());
+                    HandleMatrix = new int[N, N];
+                    rotateCounter = 0;
+                    #region null matrix
+                    for (int i = 0; i < N; i++)
+                    {
+                        for (int j = 0; j < N; j++)
+                        {
+                            HandleMatrix[i, j] = 0;
+                        }
+                    }
+                    #endregion
+                    CreateHandles(N);
+                    RandomRun();
+
+                    restartMatrixCounter++;
+                    if (restartMatrixCounter > 0)
+                    {
+                        counterOfWinsMatrix = 0;
+                        labelWins.Text = $"Пройдено: {counterOfWinsMatrix}";
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private bool ScanSafeHandles(int[,] matrix)
+        {
+            int WinCounter = 0;
+            foreach (int el in matrix)
+                if (el == 1)
+                    WinCounter++;
+            if (WinCounter == 0 || WinCounter == N * N)
+                return true;
+            else
+                return false;
+        }
+
+        private void SafeHandles_Click(object sender, EventArgs e)
+        {
+            N = Int32.Parse(tbN.Value.ToString());
+
+            PictureBox clickedPb = sender as PictureBox;
+            string str = clickedPb.Name;
+            int f, s;
+            f = Int32.Parse(str.Substring(str.Length - 2, 1));
+            s = Int32.Parse(str.Substring(str.Length - 1));
+
+            #region rotate in matrix
+            for (int i = 0; i < N; i++)
+            {
+                if (HandleMatrix[f, i] == 0)
+                    HandleMatrix[f, i] = 1;
+                else
+                    HandleMatrix[f, i] = 0;
+
+                if (HandleMatrix[i, s] == 0)
+                    HandleMatrix[i, s] = 1;
+                else
+                    HandleMatrix[i, s] = 0;
+            }
+            if (HandleMatrix[f, s] == 0)
+                HandleMatrix[f, s] = 1;
+            else
+                HandleMatrix[f, s] = 0;
+            #endregion
+
+            #region row & column visual rotate
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (HandleMatrix[i,j] == 0)
+                    {
+                        MainPanel.Controls[$"SafeHandle{i}{j}"].BackgroundImage = Properties.Resources.LineHor;
+                        MainPanel.Controls[$"SafeHandle{i}{j}"].BackgroundImage.Tag = "LineHor";
+                    }
+                    else
+                    {
+                        MainPanel.Controls[$"SafeHandle{i}{j}"].BackgroundImage = Properties.Resources.LineVer;
+                        MainPanel.Controls[$"SafeHandle{i}{j}"].BackgroundImage.Tag = "LineVer";
+                    }
+                }
+            }
+            #endregion
+
+            #region win scan
+            if (ScanSafeHandles(HandleMatrix))
+            {
+                PlaySound.Play();
+                RandomRun();
+                rotateCounter = 0;
+                labelRotates.Text = $"Проворотов: {rotateCounter}";
+                counterOfWinsMatrix++;
+                labelWins.Text = $"Пройдено: {counterOfWinsMatrix}";
+                restartMatrixCounter = 0;
+            }
+            else
+            {
+                rotateCounter++;
+                labelRotates.Text = $"Проворотов: {rotateCounter}";
+            }
+            #endregion
+        }
+
+        private void RandomRun()
+        {
+            N = Int32.Parse(tbN.Value.ToString());
+            Random rng = new Random();
+            int f, s;
+            for (int o = 0; o < 7; o++)
+            {
+                f = rng.Next(N);
+                s = rng.Next(N);
+
+                #region rotate in matrix
+                for (int i = 0; i < N; i++)
+                {
+                    if (HandleMatrix[f, i] == 0)
+                        HandleMatrix[f, i] = 1;
+                    else
+                        HandleMatrix[f, i] = 0;
+
+                    if (HandleMatrix[i, s] == 0)
+                        HandleMatrix[i, s] = 1;
+                    else
+                        HandleMatrix[i, s] = 0;
+                }
+                if (HandleMatrix[f, s] == 0)
+                    HandleMatrix[f, s] = 1;
+                else
+                    HandleMatrix[f, s] = 0;
+                #endregion
+            }
+
+            #region row & column visual rotate
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (HandleMatrix[i, j] == 0)
+                    {
+                        MainPanel.Controls[$"SafeHandle{i}{j}"].BackgroundImage = Properties.Resources.LineHor;
+                        MainPanel.Controls[$"SafeHandle{i}{j}"].BackgroundImage.Tag = "LineHor";
+                    }
+                    else
+                    {
+                        MainPanel.Controls[$"SafeHandle{i}{j}"].BackgroundImage = Properties.Resources.LineVer;
+                        MainPanel.Controls[$"SafeHandle{i}{j}"].BackgroundImage.Tag = "LineVer";
+                    }
+                }
+            }
+            #endregion
+        }
+
+        private void CreateHandles(int n)
+        {
+            int f = 0, s = 0;
+            x = 10; y = 75;
+            for (int i = 1; i <= n * n; i++)
+            {
+                // create pb
+                x += 60;
+                pBox = new PictureBox();
+                pBox.Name = "SafeHandle" + f + s;
+                pBox.Left = x;
+                pBox.Top = y;
+                pBox.Height = 50;
+                pBox.Width = 50;
+                pBox.BackgroundImageLayout = ImageLayout.Stretch;
+                pBox.BackgroundImage = Properties.Resources.LineHor;
+                pBox.BackgroundImage.Tag = "LineHor";
+
+                // putn in panel
+                MainPanel.Controls.Add(pBox);
+
+                // add click event
+                pBox.Click += new EventHandler(SafeHandles_Click);
+
+                // edit name & position
+                if (i % n == 0)
+                {
+                    x = 10;
+                    y += 60;
+                    f++;
+                    s = 0;
+                }
+                else
+                    s++;
+            }
+        }
+
+        #region change Align
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            CenterControlInParent(PanelTopic);
+        }
+
+        private void CenterControlInParent(Control ctrlToCenter)
+        {
+            ctrlToCenter.Left = (ctrlToCenter.Parent.Width - ctrlToCenter.Width) / 2;
+            ctrlToCenter.Top = 5;
+        }
+        #endregion
+    }
+}
